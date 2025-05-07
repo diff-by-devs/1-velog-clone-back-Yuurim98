d26a4e76912b2ae662690b761c1bf595bb78d296
yurim <y1551463@gmail.com>
2025-05-07 18:16:54 +0900

    init : 프로젝트 생성

---
+++ src/test/java/com/diffbydevs/velog_clone/VelogCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diffbydevs.velog_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
