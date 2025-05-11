d4808fb7ff64085c39aa1158aa58f4f0d222cc1d
yurim <y1551463@gmail.com>
2025-05-11 19:41:19 +0900

    Refactor: 이름 수정 #4

--- src/main/java/com/diffbydevs/velog_clone/common/exception/ErrorCode.java
+++ src/main/java/com/diffbydevs/velog_clone/common/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다."),
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     ACCOUNT_CONFLICT(HttpStatus.CONFLICT, "이미 사용 중인 계정입니다."),
-    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+    PASSWORD_CONFIRM_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
